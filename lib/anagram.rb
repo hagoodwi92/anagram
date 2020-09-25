802fa65d2ca7a7d8a006d9b711c7fbd93fb9c723
Alex Goodwin <hagoodwi@gmail.com>
2020-09-25 09:57:39 -0700

    add 2nd & 3rd passing test for check_if_word method

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,16 +2,34 @@ require('pry')
 class Anagram
   def word_check(word1, word2)
     counter = 0
+    word1 = word1.downcase
+    word2 = word2.downcase
     word1 = word1.split("")
-    word1.each do |letter|
-      if(word2.include? letter)
-        counter +=1
+    word2 = word2.split("")
+    word1.each do |letter1|
+      word2.each do |letter2|
+        if (letter1 == letter2)
+          counter += 1
+        end
       end
     end
-    if (counter == word2.length())
+    if (counter == word1.length() && counter == word2.length())
+      return "Anagrams!"
+    else
+      return "Not a match!"
+    end 
+  end
+  def check_if_word(word1, word2)
+    if ((word1.include? "a")| (word1.include? "i") | (word1.include? "e") | (word1.include? "o") | (word1.include? "u") | (word1.include? "y"))
+      result1 = true
+    end
+    if ((word2.include? "a")| (word2.include? "i") | (word2.include? "e") | (word2.include? "o") | (word2.include? "u") | (word2.include? "y"))
+      result2 = true
+    end
+    if ((result1 == true) & (result2 == true))
       return true
     else
-      return false
+      return "Not a real word."
     end
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -2,9 +2,16 @@ require('rspec')
 require('anagram')
 
 describe("anagram#word_check") do
-  it("returns false if words are not anagrams") do
+  it("returns message if words are not anagrams") do
     words = Anagram.new()
-    expect(words.word_check("arising","raginis")).to(eq(true))
+    expect(words.word_check("acres","cares")).to(eq("Anagrams!"))
   end
-
+  it("returns true if it is an anagram, case insensitive") do
+    words = Anagram.new()
+    expect(words.word_check("Aridest","tardies")).to(eq("Anagrams!"))
+  end
+  it("checks for errors if words are not real words") do
+    words = Anagram.new()
+    expect(words.check_if_word("ruby","ruby")).to(eq(true))
+  end
 end
